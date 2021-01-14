9b36ac0090002c2d71225f6115872483dc12147d
ratchanon6330300712 <77091543+ratchanon6330300712@users.noreply.github.com>
2021-01-14 16:43:13 +0700

    Update Cinema.java

--- lect02/src/cinema/Cinema.java
+++ lect02/src/cinema/Cinema.java
@@ -1,13 +1,60 @@
+//colaboratory 
+//1.6330300712 รัชชานนท์
+
 package cinema;
 
-public class Cinema {
+import java.util.Scanner;
 
-  void soldTickets() {
+public class Cinema {
+  final static Scanner scanner = new Scanner(System.in)
+
+  static void soldTickets() {
     // คำนวณรายได้จากการขายตั๋ว
+      int numRow;
+      int seatPerRow;
+      System.out.println("จำนวนแถวที่นั่ง :");
+      System.out.print("> ");
+      numRow = scanner.nextInt();
+
+      System.out.println("จำนวนที่นั่งแต่ละแถว :");
+      System.out.print("> ");
+      seatPerRow = scanner.nextInt();
+      int numSeats = numRow * seatPerRow;
+      int income;
+
+      if(numSeats < 60){
+        income = numSeats * 100;
+      }else {
+        int front = numRow / 2;
+        int back = numRow - front;
+        income = (front * seatPerRow * 100 )+ (back * seatPerRow * 80);
+      }
+      System.out.println("รายได้จากการจำหน่ายตั๋ว :");
+      System.out.println(income+ "บาท");
   } 
 
-  void showSeats(int numRow, int numCol) {
+  static void showSeats(int numRow, int numCol) {
     // แสดงข้อมูลที่นั่ง
+    System.out.println("Cinema: ");
+    System.out.print(" ");
+    for(int i=1; i<=numCol; i++){
+      System.out.print();
+
+      char[][] seats = new char[numRow][numCol];
+
+      for (int i=0; i< numRow; i++){
+        for (int j=0; j<numCol; j++){
+          seats[i][j] = 'S';
+        }
+      }
+      for (int i = 0; i < numRow; i++){
+        System.out.print((i + 1 +" "));
+        for (int j = 0; j < numCol; j++){
+          System.out.print(i+1+" ");
+        }
+        System.out.println();
+      }
+    }
   }
   public static void main(String[] args) {
     showSeats( 7, 8);
